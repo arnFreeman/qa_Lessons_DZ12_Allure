17689a2f010a51e463e1668859adeb410cdc967c
RenatAbdullin <arn_freeman@mail.ru>
2024-03-13 00:29:25 +0500

    add new test

---
+++ src/test/java/JitHubRepositoryIssueTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.SetValueOptions.withText;
+import static org.openqa.selenium.By.linkText;
+
+public class JitHubRepositoryIssueTest {
+
+    @DisplayName("Тест на проверку названия Issue в репозитории через Web-интерфейс")
+    @Test
+    @DisplayName("Чистый Selenide (с Listener)")
+    void checkIssueTabWithListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com");
+        $("[placeholder='Search or jump to...']").click();
+        $("#query-builder-test").setValue("arnFreeman/qa_Lessons_DZ8_PracticeFormPOWithTDG").pressEnter();
+        $(linkText("arnFreeman/qa_Lessons_DZ8_PracticeFormPOWithTDG")).click();
+        $("#issues-tab").click();
+        $((WebElement) withText("#2")).should(Condition.exist);
+    }
+
+}
